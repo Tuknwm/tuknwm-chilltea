7f56b80cb47c2ffaa886d5c679ad434ff2a8cc11
Joe Nickson Lie <joenks321@gmail.com>
2026-04-07 00:10:01 +0700

    V2

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
@@ -9,10 +9,6 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smooth: true,
-      mouseMultiplier: 1,
       touchMultiplier: 2,
       infinite: false,
     });
